bf5a13e5bf989da265904240c3027775bcd75b4e
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-06-24 10:23:00 +0530

    Update __init__.py

--- Elevenyts/__init__.py
+++ Elevenyts/__init__.py
@@ -37,7 +37,7 @@ logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
-logger = logging.getLogger("Elevenyts")
+logger = logging.getLogger("Juno X Music")
 
 # Version
 __version__ = "3.0.1"
